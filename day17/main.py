874352b7f6b82a577addb65aaa730b6e6af229f6
Marco Dijkslag <m.dijkslag@student.utwente.nl>
2020-12-17 10:27:47 +0100

    refactor, code duplication is bad

--- day17/main.py
+++ day17/main.py
@@ -11,8 +11,43 @@ TEST_CUBES = """.#.
 
 def find_neighbors(position: tuple) -> set:
     offsets = product((-1, 0, 1), repeat=len(position))
-    neighbors = {tuple(a + b for a, b in zip(position, offset)) for offset in offsets if offset != (0,) * len(position)}
-    return neighbors
+    return {tuple(a + b for a, b in zip(position, offset)) for offset in offsets if offset != (0,) * len(position)}
+
+
+def cycle_cubes(cubes_active: dict, num_cycles: int = 6) -> dict:
+    """ Returns dict of active cubes after `num_cycles` cycles"""
+    cycle = 0
+    while cycle < num_cycles:
+        becomes_inactive = set()
+        becomes_active = set()
+
+        to_add = set(cubes_active.keys())
+        for cube_pos in cubes_active.keys():
+            neighbors = find_neighbors(cube_pos)
+            for neighbor in neighbors:
+                to_add.add(neighbor)
+
+        for cube_pos in to_add:
+            neighbors = find_neighbors(cube_pos)
+            n_active_count = 0
+
+            for neighbor in neighbors:
+                if neighbor in cubes_active:
+                    n_active_count += 1
+                    if n_active_count > 3:
+                        break
+
+            if cube_pos in cubes_active and n_active_count != 2 and n_active_count != 3:
+                becomes_inactive.add(cube_pos)
+            if cube_pos not in cubes_active and n_active_count == 3:
+                becomes_active.add(cube_pos)
+
+        for cube_pos in becomes_inactive:
+            del cubes_active[cube_pos]
+        for cube_pos in becomes_active:
+            cubes_active[cube_pos] = ACTIVE
+        cycle += 1
+    return cubes_active
 
 
 def solution():
@@ -28,39 +63,7 @@ def solution():
             if cube == '#':
                 cubes_active[(i, j, 0)] = ACTIVE
 
-    cycle = 0
-    while cycle < 6:
-        becomes_inactive = defaultdict(int)
-        becomes_active = defaultdict(int)
-
-        to_add = set()
-        for cube_pos in cubes_active.keys():
-            neighbors = find_neighbors(cube_pos)
-            for neighbor in neighbors:
-                to_add.add(neighbor)
-        to_add = to_add.union(set(cubes_active.keys()))
-
-        for cube_pos in to_add:
-            neighbors = find_neighbors(cube_pos)
-            n_active_count = 0
-
-            for neighbor in neighbors:
-                if neighbor in cubes_active:
-                    n_active_count += 1
-                if n_active_count > 3:
-                    break
-
-            if cube_pos in cubes_active and n_active_count != 2 and n_active_count != 3:
-                becomes_inactive[cube_pos] = INACTIVE
-            if cube_pos not in cubes_active and n_active_count == 3:
-                becomes_active[cube_pos] = ACTIVE
-
-        for cube_pos in becomes_inactive.keys():
-            del cubes_active[cube_pos]
-        for cube_pos in becomes_active.keys():
-            cubes_active[cube_pos] = ACTIVE
-        cycle += 1
-
+    cubes_active = cycle_cubes(cubes_active)
     print("1:", len(cubes_active))
 
     # PART TWO
@@ -70,39 +73,7 @@ def solution():
             if cube == '#':
                 cubes_active[(i, j, 0, 0)] = ACTIVE
 
-    cycle = 0
-    while cycle < 6:
-        becomes_inactive = defaultdict(int)
-        becomes_active = defaultdict(int)
-
-        to_add = set()
-        for cube_pos in cubes_active.keys():
-            neighbors = find_neighbors(cube_pos)
-            for neighbor in neighbors:
-                to_add.add(neighbor)
-        to_add = to_add.union(set(cubes_active.keys()))
-
-        for cube_pos in to_add:
-            neighbors = find_neighbors(cube_pos)
-            n_active_count = 0
-
-            for neighbor in neighbors:
-                if neighbor in cubes_active:
-                    n_active_count += 1
-                if n_active_count > 3:
-                    break
-
-            if cube_pos in cubes_active and n_active_count != 2 and n_active_count != 3:
-                becomes_inactive[cube_pos] = INACTIVE
-            if cube_pos not in cubes_active and n_active_count == 3:
-                becomes_active[cube_pos] = ACTIVE
-
-        for cube_pos in becomes_inactive.keys():
-            del cubes_active[cube_pos]
-        for cube_pos in becomes_active.keys():
-            cubes_active[cube_pos] = ACTIVE
-        cycle += 1
-
+    cubes_active = cycle_cubes(cubes_active)
     print("2:", len(cubes_active))
 
 
